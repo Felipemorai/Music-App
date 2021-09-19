2be3a03875fb3cf38156349857b52f69d3a3ff95
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-09-19 15:50:37 -0300

    Upgrade

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Doja Cat - Streets",
+        artist: "Doja Cat",
+        img: "Doja Cat - Streets1.jpg",
+        src: "Doja Cat - Streets Audio"
+    },
+    {
+        name: "Doja Cat - Been like this",
+        artist: "Doja Cat",
+        img: "Doja Cat - Been like this1.jpg",
+        src: "Doja Cat - Been Like This Visualizer"
+    },
+    {
+        name: "Labrinth - Forever",
+        artist: "Labrinth",
+        img: "Labrinth - Forever1.jpg",
+        src: "Labrinth - Forever Official"
+    },
+    {
+        name: "Labrinth - Formula",
+        artist: "Labrinth",
+        img: "Labrinth - Formula1.jpg",
+        src: "Labrinth - Formula Official"
+    },
+    {
+        name: "Labrinth - We all knew",
+        artist: "Labrinth",
+        img: "Labrinth - We All Knew1.jpg",
+        src: "Labrinth - We All Knew Official"
+    },
+]
