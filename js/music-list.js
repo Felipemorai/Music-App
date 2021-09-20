08ea0fbf751b6bc27a66aa79b7a934680b79c6cd
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-09-20 16:51:25 -0300

    Upgrade

--- js/music-list.js
+++ js/music-list.js
@@ -3,30 +3,30 @@ let allMusic = [
         name: "Doja Cat - Streets",
         artist: "Doja Cat",
         img: "Doja Cat - Streets1.jpg",
-        src: "Doja Cat - Streets Audio"
+        src: "Doja Cat - Streets Audio.mp3"
     },
     {
         name: "Doja Cat - Been like this",
         artist: "Doja Cat",
         img: "Doja Cat - Been like this1.jpg",
-        src: "Doja Cat - Been Like This Visualizer"
+        src: "Doja Cat - Been Like This Visualizer.mp3"
     },
     {
         name: "Labrinth - Forever",
         artist: "Labrinth",
         img: "Labrinth - Forever1.jpg",
-        src: "Labrinth - Forever Official"
+        src: "Labrinth - Forever Official.mp3"
     },
     {
         name: "Labrinth - Formula",
         artist: "Labrinth",
         img: "Labrinth - Formula1.jpg",
-        src: "Labrinth - Formula Official"
+        src: "Labrinth - Formula Official.mp3"
     },
     {
         name: "Labrinth - We all knew",
         artist: "Labrinth",
         img: "Labrinth - We All Knew1.jpg",
-        src: "Labrinth - We All Knew Official"
+        src: "Labrinth - We All Knew Official.mp3"
     }
 ]
--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@
 const wrapper = document.querySelector(".wrapper"),
 musicImg = wrapper.querySelector(".img-area img"),
 musicName = wrapper.querySelector(".song-details .name"),
-musicArtist = wrapper.querySelector(".song-details .artist");
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio");
 
-let musicIndex = 2;
+let musicIndex = 1;
 
 window.addEventListener("load", () => {
-    /* Calling load music function once window loaded */
+/* Calling load music function once window loaded */
     loadMusic(musicIndex);
 })
 
 /* Load music function */
-function loadMusic() {
-
+function loadMusic(indexNumb) {
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `img/${allMusic[indexNumb - 1].img}`;
+    mainAudio.src = `music/${allMusic[indexNumb - 1].src}`;
 }
