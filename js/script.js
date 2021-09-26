df60d98b4a30f749e6a2d086d13ba1c32a338a08
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-09-26 10:00:54 -0300

    Upgrade

--- js/script.js
+++ js/script.js
@@ -180,29 +180,13 @@ const ulTag = wrapper.querySelector("ul");
 
 /* Creating li according to the array length */
 for (let i = 0; i < allMusic.length; i++) {
-    /* Passing the song name, artist from the array to li */
-    let liTag = `
-    <li li-index="${i + 1}">
-      <div class="row">
-        <span>${allMusic[i].name}</span>
-        <p>${allMusic[i].artist}</p>
-      </div>
-      <audio class="${allMusic[i].src}" src="./music/${allMusic[i].src}"></audio>
-      <span id="${allMusic[i].src}" class="audio-duration">3:47</span>
-    </li>`;
+    /* Will pass the song name, artist from the array to li*/
+    let liTag = `<li>
+                  <div class="row">
+                    <span>Doja Cat - Streets</span>
+                    <p>Doja Cat</p>
+                  </div>
+                  <span class="audio-duration">3:40<span>
+                 </li>`;
     ulTag.insertAdjacentHTML("beforeend", liTag);
-
-    let liAudioDuartionTag = ulTag.querySelector(`#${allMusic[i].src}`);
-    let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`);
-
-    liAudioTag.addEventListener("loadeddata", () => {
-        let duration = liAudioTag.duration;
-        let totalMin = Math.floor(duration / 60);
-        let totalSec = Math.floor(duration % 60);
-        if(totalSec < 10) { /* Adding 0 if sec is less than 10 */
-            totalSec = `0${totalSec}`;
-        }
-        liAudioDuartionTag.innerText = `${totalMin}:${totalSec}`;
-        liAudioDuartionTag.setAttribute("t-duration", `${totalMin}:${totalSec}`)
-    });
 }
